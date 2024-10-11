e908cc1da3f785d4cd1fd0644a2d56edf002e7fd
William Liu <orangetnt827@gmail.com>
2024-10-10 20:11:57 -0500

    change config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.html"],
-  theme: {
-    extend: {
-      colors: {
-        dark1: "#0284c7"
-      }
-    },
-  },
-  plugins: [],
 }
-
