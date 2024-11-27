95aa85c9369917d6de9d71a22fce5a6f427e4b97
William Liu <orangetnt827@gmail.com>
2024-11-26 21:11:54 -0600

    remove vite, change eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import globals from 'globals';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   prettier,
@@ -11,7 +10,11 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
+        $: 'readonly',
       },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
     },
   },
 ];
